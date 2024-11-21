3d9062cfec93b102d1bb88a4921c1d31c9fe5ce2
dorffdaniel <danielcontabill@hotmail.com>
2024-11-21 20:55:12 +0000

    primeiro commit

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+let Imagens= ["src/imagens/interior1.jpg", "src/imagens/interior2.jpg"];
+
+let indice = 0;
+
+function trocaDasImagens(){
+    indice ++;
+    if(indice >= Imagens.length){
+        indice =0;
+    }
+    document.querySelector('.troca-imagens').src=Imagens[indice];
+
+}
+
+setInterval(trocaDasImagens, 4000)
+
+
+const btn = document.querySelector(".btn").addEventListener('click', ()=>{
+    const cards = document.querySelector(".cards")
+
+    cards.classList.add('ativarcards');
+    
+    btn.classList.remove('animacao');
+    btn.classList.add('clicado');
+})
